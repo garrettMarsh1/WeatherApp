525ef723df03b96fb5fff952d3ffe6056187e64c
Garrett Marsh <garrett.marsh95@gmail.com>
2022-04-21 23:58:46 -0400

    adding files

---
+++ AppScript.js
@@ -0,0 +1,23 @@
+let weather = {
+    apiKey: "884429938c4e95cca92dcc3032645b85",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+             + city 
+             + "&units=imperial&appid=" 
+             + this.apiKey   
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector("city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+            
+    }
+};    
